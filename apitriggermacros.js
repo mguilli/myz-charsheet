28def57d956c92d387666fd7fe0ecf42129b0d3d
Matthew Guilliams <matthew.guilliams@gmail.com>
2016-03-26 16:46:45 -0500

    added listing of api triggering macros

---
+++ apitriggermacros.js
@@ -0,0 +1,15 @@
+// Skill API macros
+!myz [[(?{Modification|0}+@{force})d6]] [[(@{strength}-@{damage})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Force (STR)
+!myz [[(?{Modification|0}+@{fight})d6]] [[(@{strength}-@{damage})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Fight (STR)
+!myz [[(?{Modification|0}+@{sneak})d6]] [[(@{agility}-@{fatigue})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Sneak (AGL)
+!myz [[(?{Modification|0}+@{move})d6]] [[(@{agility}-@{fatigue})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Move (AGL)
+!myz [[(?{Modification|0}+@{shoot})d6]] [[(@{agility}-@{fatigue})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Shoot (AGL)
+!myz [[(?{Modification|0}+@{scout})d6]] [[(@{wits}-@{confusion})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Scout (WIT)
+!myz [[(?{Modification|0}+@{comprehend})d6]] [[(@{wits}-@{confusion})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Comprehend (WIT)
+!myz [[(?{Modification|0}+@{knowthezone})d6]] [[(@{wits}-@{confusion})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Know the Zone (WIT)
+!myz [[(?{Modification|0}+@{senseemotion})d6]] [[(@{empathy}-@{doubt})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Sense Emotion (EMP)
+!myz [[(?{Modification|0}+@{manipulate})d6]] [[(@{empathy}-@{doubt})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Manipulate (EMP)
+!myz [[(?{Modification|0}+@{heal})d6]] [[(@{empathy}-@{doubt})d6]] [[?{Gear Dice|0}d6]] --Player|@{character_name} --Skill|Heal (EMP)
+
+//Repeating section skill macro
+!myz [[]]
