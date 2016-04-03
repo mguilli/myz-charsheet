5387379459f966902c286f781525dfea5ab4f4b6
Matthew Guilliams <matthew.guilliams@gmail.com>
2016-04-03 16:38:31 -0500

    Styled buttons and added functionality to change rot resource inventory

--- API Scripts/rotTracker.js
+++ API Scripts/rotTracker.js
@@ -11,119 +11,159 @@ mguilli.rotten = (function () {
       args = msg.content.trim().splitArgs(),
       command, arg0, arg1;
 
-      // Initialize rot inventory on state global object
-      state.rot = state.rot || {};
+    var buttonStyle = {
+      default: 'style="-webkit-border-radius: 5;-moz-border-radius: 5;border-radius: 5px;font-family: Arial;color: #ffffff;font-size: 12px;background:dodgerblue;padding: 2px 7px 2px 7px;border: solid black 1px;text-decoration: none;"',
+      dark: 'style="-webkit-border-radius: 5;-moz-border-radius: 5;border-radius: 5px;font-family: Arial;color: #ffffff;font-size: 12px;background:black;padding: 2px 7px 2px 7px;border: solid black 1px;text-decoration: none;"',
+      danger: 'style="-webkit-border-radius: 5;-moz-border-radius: 5;border-radius: 5px;font-family: Arial;color: #ffffff;font-size: 12px;background:crimson;padding: 2px 7px 2px 7px;border: solid black 1px;text-decoration: none;"'
+    };
 
-      //template for output to gm chat window
-      var messageOut = _.template("/w gm "+
-        "<div style='border: 1px solid black; background-color: white; padding: 3px 3px;'>"+
-          "<%= output %>"+
-        "</div");
+    // Button styler: makeButton({link: href command, css: button.default, display: text to display});
+    var makeButton = _.template('<a href="<%= link %>" <%= css %>><%= display %></a> ');
+
+    // Initialize rot inventory on state global object
+    state.rot = state.rot || {};
+
+    // Match rot resource to general resource type
+    var resource = {rotGrub: 'grub', rotWater: 'water'};
+
+    //template for output to gm chat window
+    var messageOut = _.template("/w gm "+
+      "<div style='border: 1px solid black; background-color: white; padding: 3px 3px;'>"+
+        "<%= output %>"+
+      "</div");
 
 
-      if (isApi) {
-        command = args.shift().substring(1).toLowerCase();
-        arg0 = args.shift() || '';
-        arg1 = args.shift() || '';
-        arg2 = args.shift() || '';
-        var output = '';
+    if (isApi) {
+      command = args.shift().substring(1).toLowerCase();
+      arg0 = args.shift() || '';
+      arg1 = args.shift() || '';
+      arg2 = args.shift() || '';
+      var output = '';
 
-        log('command = '+command+' arg0 = '+arg0+' arg1= '+arg1+' arg2= '+arg2);
+      log('command = '+command+' arg0 = '+arg0+' arg1= '+arg1+' arg2= '+arg2);
 
-        if (command === 'rot') {
-          // Make characters eligible to receive rot-infested water and grub
-          if (arg0 === '--roster') {
-            //retrieve all characters in game
-            var allCharacters = findObjs({_type: 'character'});
+      if (command === 'rot') {
+        // Make characters eligible to receive rot-infested water and grub
+        if (arg0 === '--roster') {
+          //retrieve all characters in game
+          var allCharacters = findObjs({_type: 'character'});
 
-            _.each(allCharacters,function (char) {
-              log(char.get('name')+" "+char.id);
-              if (state.rot[char.id]) {
-                output += '<p>'+char.get('name')+' [Remove](!rot --rosterremove '+char.id+')'+'</p>';
-              } else{
-                output += '<p>'+char.get('name')+' [Add](!rot --rosteradd '+char.id+')'+'</p>';
-              }
-            });
-            sendChat("",messageOut({output: output}));
+          _.each(allCharacters,function (char) {
+            log(char.get('name')+" "+char.id);
+            if (state.rot[char.id]) {
+              output += '<p>'+char.get('name')+' '+makeButton({link: '!rot --rosterremove '+char.id, css: buttonStyle.danger, display: 'Remove'})+'</p>';
+            } else{
+              output += '<p>'+char.get('name')+' '+makeButton({link: '!rot --rosteradd '+char.id, css: buttonStyle.default, display: 'Add'})+'</p>';
+            }
+          });
+          sendChat("",messageOut({output: output}));
+        };
+
+        if (arg0 === '--rosteradd') {
+          if(!arg1) {
+            sendChat('Error', '/w gm No character specified!');
+            return;
           };
 
-          if (arg0 === '--rosteradd') {
-            if(!arg1) {
-              sendChat('Error', '/w gm No character specified!');
-              return;
-            };
+          if (!state.rot.hasOwnProperty(arg1)) {
+            state.rot[arg1] = {};
+            state.rot[arg1]['name'] = getAttrByName(arg1,'character_name');
+            
+            log('Name = '+getAttrByName(arg1, 'character_name'));
 
-            if (!state.rot.hasOwnProperty(arg1)) {
-              state.rot[arg1] = {};
-              state.rot[arg1]['name'] = getAttrByName(arg1,'character_name');
-              
-              log('Name = '+getAttrByName(arg1, 'character_name'));
-
-              state.rot[arg1].rotWater = 0;
-              state.rot[arg1].rotGrub = 0;
-              sendChat("Success",'/w gm '+getAttrByName(arg1,'character_name')+" has been added to the Rot Roster.");
-            };
-          };
-
-          if (arg0 === '--rosterremove') {
-            if (!arg1) {
-              sendChat('Error', '/w gm No character specified!');
-              return;
-            };
-
-            if(state.rot.hasOwnProperty(arg1)) {
-              delete state.rot[arg1];
-              sendChat("Success",'/w gm '+getAttrByName(arg1,'character_name')+" has been removed from the Rot Roster.");
-            };
-
-          };
-
-          // View rot-infested to total grub/water
-          if (arg0 === '--inventory') {
-            if (_.isEmpty(state.rot)) {
-              sendChat('Error', '/w gm No characters have been added to the Rot Roster yet!');
-              return;
-            };
-
-            //Print out all characters in Rot Roster with action button options
-            _.each(state.rot, function (char, key) {
-              output += '<p><strong>'+char.name+':</strong></p>'+
-                        '<p>  Grub: '+char.rotGrub+'/'+getAttrByName(key, 'grub')+' [Consume](!consume) [Add/Rem](!controller --change rotGrub '+key+')</p>'+
-                        '<p>  Water: '+char.rotWater+'/'+getAttrByName(key, 'water')+' [Consume](!consume) [Add/Rem](!change)</p>';
-            });
-
-            sendChat('',messageOut({output: output}));
+            state.rot[arg1].rotWater = 0;
+            state.rot[arg1].rotGrub = 0;
+            sendChat("Success",'/w gm '+getAttrByName(arg1,'character_name')+" has been added to the Rot Roster."+
+              makeButton({link: '!rot --roster', css: buttonStyle.default, display: 'Roster'}));
           };
         };
 
-        // Change a character's Rot inventory
-        // Example Format: !change rotGrub characterID newvalue
-        // Where:
-        // arg0 = resource type; arg1 = characterID; arg2 = newvalue
-        // if (command === 'change') {
-        //   if (arg0 === 'rotGrub' || arg0 === 'rotWater' && arg1 && arg2) {
-        //     log("succesful inventory change command");
-        //     state.rot[arg1][arg0] = arg2;
-        //     sendChat('Success', '/w gm '+state.rot[arg1].name"'s "+arg0+" was changed to: "+arg2);
+        if (arg0 === '--rosterremove') {
+          if (!arg1) {
+            sendChat('Error', '/w gm No character specified!');
+            return;
+          };
 
-        //   } else {
-        //     sendChat('Error','/w gm Invalid number of arguments.');
-        //   };
-        // };
+          if(state.rot.hasOwnProperty(arg1)) {
+            delete state.rot[arg1];
+            sendChat("Success",'/w gm '+getAttrByName(arg1,'character_name')+" has been removed from the Rot Roster."+
+              makeButton({link: '!rot --roster', css: buttonStyle.default, display: 'Roster'}));
+          };
 
-        // sendChat functions to prompt user input
-        // Example format: !controller --change resourceType charID
-        // if (command === 'controller') {
-        //   if (arg0 === '--change' && arg0 && arg1 && arg2) {
-        //     log("Success on change trigger");
-        //     // sendChat('',"!change "+arg1+" "+arg2+" ?{Change "+arg1+" to: |"+state.rot[arg2][arg1]+'}');
-        //     sendChat('', 'You hit the button!!!');
-        //     sendChat('', '!change ?{Test selection: |2}');
-        //   } else {
-        //     sendChat('Error','/w gm Invalid number of arguments.');
-        //   };
-        // }; 
+        };
+
+        // View rot-infested to total grub/water
+        if (arg0 === '--inventory') {
+          if (_.isEmpty(state.rot)) {
+            sendChat('Error', '/w gm No characters have been added to the Rot Roster yet!');
+            return;
+          };
+
+          //Print out all characters in Rot Roster with action button options
+          _.each(state.rot, function (char, key) {
+            output += '<div style="border-top: 1px solid black"><p><strong>'+char.name+':</strong></p>'+
+                      '<p>  Grub: '+char.rotGrub+'/'+getAttrByName(key, 'grub')+' '+                          
+                        makeButton({link: '!consume', 
+                          css: buttonStyle.danger, 
+                          display: 'Use'})+
+                        makeButton({link: '!rot-change rotGrub '+key+' ?{New Value: |'+char.rotGrub+'}', 
+                          css: buttonStyle.default, 
+                          display: 'Set Rot'})+
+                        makeButton({link: '!rot-add',
+                          css: buttonStyle.dark,
+                          display: '+/-'})+
+                      '</p>'+
+                      '<p>  Water: '+char.rotWater+'/'+getAttrByName(key, 'water')+' '+
+                        makeButton({link: '!consume', 
+                          css: buttonStyle.danger, 
+                          display: 'Use'})+
+                        makeButton({link: '!rot-change rotWater '+key+' ?{New Value: |'+char.rotWater+'}', 
+                          css: buttonStyle.default, 
+                          display: 'Set Rot'})+
+                        makeButton({link: '!rot-add',
+                          css: buttonStyle.dark,
+                          display: '+/-'})+
+                      '</p></div>';
+          });
+
+          sendChat('',messageOut({output: output}));
+        };
       };
+
+      // Change a character's Rot inventory
+      // Example Format: !change rotGrub characterID newvalue
+      // Where:
+      //arg0 = resource type; arg1 = characterID; arg2 = newvalue
+      if (command === 'rot-change') {
+        if (arg0 === 'rotGrub' || arg0 === 'rotWater' && arg1 && arg2) {
+          log("succesful inventory change command");
+          var resourceTotal = getAttrByName(arg1,resource[arg0]);
+          var alertNotice = '';
+          // Set state.rot[charID][resourceType] = to new value
+          state.rot[arg1][arg0] = arg2;
+
+
+          // Alert if new rot-infested resource units are greater than total units in character's inventory
+          if (arg2 > resourceTotal) {
+            alertNotice = '<p><strong>Rot inventory greater than total inventory!!!</strong></p>';
+          };
+
+          output = state.rot[arg1].name+"'s "+arg0+" was changed to: "+arg2+'/'+resourceTotal+alertNotice+' '+
+            '<p>'+makeButton({link: '!rot --inventory', css: buttonStyle.default, display: 'Inventory'})+'</p>';
+
+          sendChat('',messageOut({output: output}));
+
+        } else {
+          sendChat('Error','/w gm Invalid number of arguments.');
+        };
+      };
+
+      // Handle consumption of rot infested resources
+      if (true) {};
+
+      // Handle increment/decrement of resources instead of setting the value
+      if (true) {};
+    };
   };
 
   function registerEventHandlers() {
--- API Scripts/temp.js
+++ API Scripts/temp.js
@@ -14,3 +14,5 @@ _.each(state.rot, function (char, key, list) {
   // console.log(key);
   console.log(list);
 })
+
+var default = 'style="-webkit-border-radius: 10;-moz-border-radius: 10;border-radius: 10px;font-family: Arial;color: #ffffff;font-size: 13px;background:#148c0e;padding: 5px 5px 5px 5px;border: solid #1bbbf5 2px;text-decoration: none;"'; 
